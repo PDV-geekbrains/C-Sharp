aa15c6f21b4022d94627ffcea1aced04fc4450b9
Dmitriy <pd2018@yandex.ru>
2023-03-06 19:41:41 +0300

    Добавлена Задача_2

---
+++ Урок_1-Задача_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nСравнение двух чисел.\n");
+
+            Console.WriteLine("Введите первое число и нажмите 'Ввод'");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число и нажмите 'Ввод'");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            if(firstNumber == secondNumber)
+                Console.WriteLine($"a = {firstNumber}, b = {secondNumber} -> a = b\n");
+            else if(firstNumber > secondNumber)
+                Console.WriteLine($"a = {firstNumber}, b = {secondNumber} -> max = {firstNumber}\n");
+            else
+                Console.WriteLine($"a = {firstNumber}, b = {secondNumber} -> max = {secondNumber}\n");
+        }
+    }
+}
