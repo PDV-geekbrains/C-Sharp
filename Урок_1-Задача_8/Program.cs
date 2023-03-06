79de74f3d26985e2b74e273e1de8cae57df4423a
Dmitriy <pd2018@yandex.ru>
2023-03-06 20:03:27 +0300

    Дополнено описание в README.md

---
+++ Урок_1-Задача_8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nВывод всех четных чисел от 1 до N.\n");
+
+            Console.WriteLine("Введите число и нажмите 'Ввод'");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{number} --> ");
+
+            if (number % 2 == 0) {
+                do
+                {
+                    Console.Write($"{number - 2}, ");
+                    number -= 2;
+                } while (number - 2 > 0);
+            }
+            else{
+                int newNum = number + 1;
+                do
+                {
+                    Console.Write($"{newNum - 2}, ");
+                    newNum -= 2;
+                } while (newNum - 2 > 0);
+            }
+        }
+    }
+}
