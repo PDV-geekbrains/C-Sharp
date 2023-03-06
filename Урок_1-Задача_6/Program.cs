d9f68f5f9f63686b016e8cb733c55c94cbf6c168
Dmitriy <pd2018@yandex.ru>
2023-03-06 19:53:10 +0300

    Добавлена Задача_6 из урока_1

---
+++ Урок_1-Задача_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nОпределение чётности числа.\n");
+
+            Console.WriteLine("Введите число и нажмите 'Ввод'");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number%2 != 0)
+                Console.WriteLine($"{number} -> нет");
+            else
+                Console.WriteLine($"{number} -> да");
+        }
+    }
+}
