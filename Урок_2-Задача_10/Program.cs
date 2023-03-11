253f749b619b58e30cc09452c8bf62b70222aa3a
Dmitriy <pd2018@yandex.ru>
2023-03-11 23:37:07 +0300

    Добавлена Задача_10 из урока_2

---
+++ Урок_2-Задача_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace L2_E10
+{
+    class Program
+    {
+        static void Main()
+        {
+          string caption = "\nВывод второй цифры 3-значного числа\n";
+          Console.WriteLine(caption.ToUpper());
+
+          Console.WriteLine("Введите трёхзначное число");
+          int num = Convert.ToInt32(Console.ReadLine());
+          if(num <0)
+                num *= -1;
+
+            Console.WriteLine($"{num} -> {(num % 100) / 10}");
+        }
+    }
+}
