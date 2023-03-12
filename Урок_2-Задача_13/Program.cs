ef51806a70f5f71504fa34268a0a9636b92ec36c
Dmitriy <pd2018@yandex.ru>
2023-03-12 13:40:56 +0300

    Добавлена Задача_13 из урока_2

---
+++ Урок_2-Задача_13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace L2_E13
+{
+    class Program
+    {
+        static void Main()
+        {
+            string caption = "\nВывод на консоль третьей слева цифры введённого числа\n";
+            Console.WriteLine(caption.ToUpper());
+
+            Console.WriteLine("Введите число");
+            string enteredString = Console.ReadLine();
+            int stringLength = enteredString.Length;
+
+            if(stringLength < 3)
+            {
+                Console.WriteLine($"{enteredString} -> третьей цифры нет");
+            }
+            else
+            {
+                int number = Convert.ToInt32(enteredString);
+
+                if (number < 0)
+                {
+                    number *= -1;
+                    stringLength -= 1;
+                }
+                System.Console.WriteLine(number);
+
+                int result = number;
+
+                for (int i = stringLength - 3; i > 0; i--)
+                {
+                    result = result / 10;
+                }
+                result %= 10;
+
+                System.Console.WriteLine($"{number} -> {result}");
+            }
+        }
+    }
+}
