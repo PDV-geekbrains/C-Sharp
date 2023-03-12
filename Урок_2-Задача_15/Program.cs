c9c0748d8c1d4adff1bae9125a4edbc4ac23b6be
Dmitriy <pd2018@yandex.ru>
2023-03-12 14:00:47 +0300

    Добавлена Задача_15 из урока_2

---
+++ Урок_2-Задача_15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace L2_E15
+{
+    class Program
+    {
+        static void Main()
+        {
+            string caption = "\nВведённый день недели - выходной?\n";
+            Console.WriteLine(caption.ToUpper());
+
+            Console.WriteLine("Введите порядковый номер дня недели: 1 - Пн. ... 7 - Вс.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 1 || number > 7)
+            {
+                Console.WriteLine("Введите число от 1 до 7 включительно");
+            }
+            else if (number < 6)
+            {
+                if (number < 6)
+                    Console.WriteLine($"{number} -> нет");
+            }
+            else
+            {
+                Console.WriteLine($"{number} -> да");
+            }
+        }
+    }
+}
