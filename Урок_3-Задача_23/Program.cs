d3c27ab25d57411ebd9f8588bb0a8f0015777a1f
Dmitriy <pd2018@yandex.ru>
2023-03-13 21:02:21 +0300

    Добавлена Задача_23 из урока_3

---
+++ Урок_3-Задача_23/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace L3_E23
+{
+    class Program
+    {
+        static void Main()
+        {
+            string caption = "\nТаблица кубов чисел от 1 до N для числа N\n";
+            Console.WriteLine(caption.ToUpper());
+
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"{number} -> ");
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write($"{Math.Pow(i, 3)} ");
+            }
+
+        }
+    }
+}
