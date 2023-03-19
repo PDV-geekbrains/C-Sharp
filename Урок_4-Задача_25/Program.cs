1d267509523c4933a5f002c3c84d7b416d72ca13
Dmitriy <pd2018@yandex.ru>
2023-03-19 21:30:54 +0300

    Добавлена Задача_25 из урока_4

---
+++ Урок_4-Задача_25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace L4_E25
+{
+    class Program
+    {
+        static void Main()
+        {
+            string caption = "\nВозведение числа А в натуральную степень В\n";
+            Console.WriteLine(caption.ToUpper());
+
+            Console.Write("Введите число А: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите число В: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            double result = a;
+
+            if(b !=0)
+            {
+                for (int i = 1; i < b; i++)
+                {
+                    result = result * a;
+                }
+                Console.Write($"{a}, {b} -> {result} ");
+            }
+            else
+            {
+                Console.Write($"{a}, {b} -> 1 ");
+            }
+        }
+    }
+}
