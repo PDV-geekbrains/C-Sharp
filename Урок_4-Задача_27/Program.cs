d5c0867febfe6c9663fb2db2160eccf18478b10a
Dmitriy <pd2018@yandex.ru>
2023-03-19 22:19:16 +0300

    Добавлена Задача_27 из урока_4

---
+++ Урок_4-Задача_27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L4_E27
+{
+    class Program
+    {
+        static void Main()
+        {
+            string caption = "\nВычисление суммы цифр в числе\n";
+            Console.WriteLine(caption.ToUpper());
+
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            int temp = number;
+            
+            while(temp >= 1)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            Console.WriteLine($"{number} -> {sum}");
+        }
+    }
+}
