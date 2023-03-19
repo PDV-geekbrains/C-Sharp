86f27b11756cbbce39bb14d4677277e23ddcd4c4
Dmitriy <pd2018@yandex.ru>
2023-03-19 23:25:02 +0300

    Добавлена Задача_29 из урока_4

---
+++ Урок_4-Задача_29/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace L4_E29
+{
+    class Program
+    {
+        static void Main()
+        {
+            string caption = "\nЗаполнение массива числами пользователя\n";
+            Console.WriteLine(caption.ToUpper());
+
+            int[] arr = new int[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                if(i == 7)
+                    Console.WriteLine("Введите последнее число");
+                else
+                    Console.WriteLine("Введите число");
+
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
+                arr[i] = inputNumber;
+
+                PrintArray(i);
+            }
+
+            void PrintArray(int maxIndex)
+            {
+                for (int i = 0; i <= maxIndex; i++)
+                {
+                    if(i == 0)
+                        Console.Write($"{arr[i]}");
+                    else
+                        Console.Write($", {arr[i]}");
+                }
+
+                Console.Write(" -> [");
+
+                for (int i = 0; i <= maxIndex; i++)
+                {
+                    if(i == 0)
+                        Console.Write($"{arr[i]}");
+                    else
+                        Console.Write($", {arr[i]}");
+                }
+                Console.Write("]\n");
+            }
+            //Console.Write($"-> [{string.Join(", ", arr)}]");
+        }
+    }
+}
