e17026f778c1c2f36ea0522775d5b18b91f4d364
Dmitriy <pd2018@yandex.ru>
2023-03-21 20:35:22 +0300

    Добавлена Задача_36 из урока_5

---
+++ Урок_5-Задача_36/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace L5_E36
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] randomArray = GetRandomNumbersArray();
+
+            int sum = GetSum(randomArray);
+
+            Console.WriteLine($"[{string.Join(", ", randomArray)}] -> {sum}");
+        }
+
+        static int[] GetRandomNumbersArray()
+        {
+            int[] array = new int[4];
+
+            for (int i = 0; i < 4; i++)
+            {
+                array[i] = new Random().Next(1, 100);
+            }
+
+            return array;
+        }
+
+        static int GetSum(int[] array)
+        {
+            int sum = 0;
+            int i = 1;
+            while( i < 4)
+            {
+                sum += array[i];
+                i += 2;
+            }
+
+            return sum;
+        }
+    }
+}
