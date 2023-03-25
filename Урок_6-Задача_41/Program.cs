41ed4300c06f3c209745cb6309a5baf8402b3332
Dmitriy <pd2018@yandex.ru>
2023-03-26 00:11:28 +0300

    Задача_41 из урока_6

---
+++ Урок_6-Задача_41/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace L6_E41
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            Console.WriteLine("Введите несколько целых чисел через пробел");
+
+            int[] numbers = Console.ReadLine().Split(" ").Select(x => Int32.Parse(x)).ToArray();
+
+            int count = 0;
+            foreach(int item in numbers)
+            {
+                if(item > 0)
+                    count++;
+            }
+
+            Console.WriteLine($"{string.Join(", ", numbers)} -> {count}");
+        }
+    }
+}
