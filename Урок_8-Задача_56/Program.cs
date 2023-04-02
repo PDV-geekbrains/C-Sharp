b6b8f07664268da3cede2195d2865f1f890f5500
Dmitriy <pd2018@yandex.ru>
2023-04-02 11:36:46 +0300

    Добавлена Задача_56 из урока_8

---
+++ Урок_8-Задача_56/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace L8_E56
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintCaption();
+            int[] arraySize = GetArraySize();
+            int[,] rndItemsArray = GetRandomItemsArray(arraySize);
+            int rowNumber = GetMinItemsSumRowNumber(rndItemsArray);
+            PrintResult(rndItemsArray, rowNumber);
+        }
+        static  void PrintCaption()
+        {
+            Console.Clear();
+            Console.WriteLine("\nНахождение строки с наименьшей суммой элементов\n");
+        }
+        static int[] GetArraySize()
+        {
+            Console.Write("Введите через пробел размер массива (M N): ");
+            return Console.ReadLine()
+                          .Split(" ")
+                          .Select(x => int.Parse(x))
+                          .ToArray();
+        }
+        static int[,] GetRandomItemsArray(int[] arraySize)
+        {
+            int[,] array = new int[arraySize[0],
+                                   arraySize[1]];
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int column = 0; column < array.GetLength(1); column++)
+                {
+                    array[row, column] = new Random().Next(10, 100);
+                }
+            }
+            return array;
+        }
+        static int GetMinItemsSumRowNumber(int[,] array)
+        {
+            int minSum = -1;
+            int rowNumber = 0;
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                int rowSum = 0;
+                for (int column = 0; column < array.GetLength(1); column++)
+                {
+                    rowSum += array[row, column];
+                }
+                if(minSum == -1)
+                    minSum = rowSum;
+                else if(minSum > rowSum)
+                {
+                    minSum = rowSum;
+                    rowNumber = row;
+                }
+            }
+            return rowNumber;
+        }
+        static void PrintResult(int[,] array, int rowNumber)
+        {
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int column = 0; column < array.GetLength(1); column++)
+                {
+                    Console.Write($"{array[row, column]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"\n Номер строки с наименьшей суммой элементов: " +
+                              $"{rowNumber}");
+        }
+    }
+}
