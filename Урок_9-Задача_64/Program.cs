0c7309fccf8550dae05ffad75cbd863e8a96afb0
Dmitriy <pd2018@yandex.ru>
2023-04-05 23:15:09 +0300

    Добавлены Задачи 64, 66, 68 из урока_9

---
+++ Урок_9-Задача_64/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace L9_E64
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintCaption();
+            int maxNumber = GetNumber();
+            string numberSequence = GetNumberSequence(maxNumber);
+            PrintResult(maxNumber, numberSequence);
+        }
+        static void PrintCaption()
+        {
+            Console.Clear();
+            Console.WriteLine("Вывод натуральных чисел в промежутке от N до 1\n");
+        }
+        static int GetNumber()
+        {
+            Console.Write("Введите натуральное число: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static string GetNumberSequence(int number)
+        {
+            if(number == 0)
+                return 0.ToString();
+            else if(number == 1)
+                return 1.ToString();
+                
+            return $"{number}, {GetNumberSequence(number - 1)}";
+        }
+        static void PrintResult(int maxNumber, string numberSequence)
+        {
+            Console.WriteLine($"N = {maxNumber} -> {numberSequence}");
+        }
+    }
+}
---
+++ Урок_9-Задача_66/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace L9_E66
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintCaption();
+            int firstNumber = GetFirstNumber();
+            int lastNumber = GetLastNumber();
+            int sum = GetSum(firstNumber, lastNumber);
+            PrintResult(firstNumber,
+                        lastNumber,
+                        sum);
+        }
+        static void PrintCaption()
+        {
+            Console.Clear();
+            Console.WriteLine("Нахождение суммы натуральных чисел в промежутке от M до N\n");
+        }
+        static int GetFirstNumber()
+        {
+            Console.Write("Введите начальное число (M): ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int GetLastNumber()
+        {
+            Console.Write("Введите конечное число (N): ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int GetSum(int number, int lastNumber)
+        {
+            if (number == lastNumber + 1)
+                return 0;
+            return number + GetSum(++number, lastNumber);
+        }
+        static void PrintResult(int firstNumber,
+                                int lastNumber,
+                                int sum)
+        {
+            Console.WriteLine($"M = {firstNumber}; " +
+                $"N = {lastNumber} -> {sum}");
+        }
+    }
+}
