7077771107b99b21c14d1888e23e17bdc19e4e61
Dmitriy <pd2018@yandex.ru>
2023-04-05 23:11:44 +0300

    Добавлены Задачи 64, 66, 68 из урока_9

---
+++ Урок_9-Задача_68/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace L9_E68
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintCaption();
+            int m = GetFirstNumber();
+            int n = GetSecondNumber();
+            int result = AckermannFunction(m, n);
+            PrintResult(m, n, result);
+        }
+        static void PrintCaption()
+        {
+            Console.Clear();
+            Console.WriteLine("Вычисления функции Аккермана с помощью рекурсии\n");
+        }
+        static int GetFirstNumber()
+        {
+            Console.Write("Введите первое неотрицательное целое число (M): ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int GetSecondNumber()
+        {
+            Console.Write("Введите второе неотрицательное целое число (N): ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int AckermannFunction(int m, int n)
+        {
+            if(m == 0)
+                return n + 1;
+            if(m > 0 && n == 0)
+                return AckermannFunction(m - 1, 1);
+            return AckermannFunction(m - 1,
+                AckermannFunction(m, n - 1));
+        }
+        static void PrintResult(int m, int n, int result)
+        {
+            Console.WriteLine($"\nm = {m}, " +
+                $"n = {n} -> A(m,n) -> {result}");
+        }
+    }
+}
